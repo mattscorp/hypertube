c7ff4d66b66798df7e7dda6e553e52636ff821c1
Paul VICTOR <p.victor@outlook.fr>
2019-10-21 19:28:43 +0200

    authentification et token dans this.stats ok

--- views/src/App.js
+++ views/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import home from './pages/Home.js';
@@ -8,10 +8,10 @@ import MainNavigation from './components/navigation/MainNavigation';
 import AuthContext from './context/auth-context';
 
 
-function App() {
+class App extends Component {
   state = {
     token: null
-  }
+  };
 
   login = (token) => {
     this.setState({token: token});
@@ -19,36 +19,35 @@ function App() {
 
   logout = () => {
     this.setState({token: null});
-  }
+  };
 
-  return (
-    <BrowserRouter>
-      <React.Fragment>
-<<<<<<< HEAD
-        <AuthContext.Provider value={{token: this.state.token, login: this.login, logout: this.logout }}>
-          <MainNavigation />
-          <main className="mt-2">
-            <Switch>
-              <Redirect from="/" to="/auth" exact/>
-              <Route path="/auth" component={ AuthPage }/>
-              <Route path="/account" component={ AccountPage }/>
-            </Switch>
-          </main>
-        </AuthContext.Provider>
-=======
-        <MainNavigation />
-        <main className="mt-2">
-          <Switch>
-            <Redirect from="/" to="/home" exact/>
-            <Route path="/auth" component={ AuthPage }/>
-            <Route path="/account" component={ AccountPage }/>
-            <Route path="/home" component={ home }/>
-          </Switch>
-        </main>
->>>>>>> 9ed3d9a424cbb33cb40a9cde085dc2b519368e94
-      </React.Fragment>
-    </BrowserRouter>
-  );
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              login: this.login,
+              logout: this.logout
+              }}>
+            <MainNavigation />
+            <main className="mt-2">
+              <Switch>
+                {this.state.token && <Redirect from="/" to="/home" exact/>}
+                {!this.state.token && <Redirect from="/" to="/auth" exact/>}
+                {this.state.token && <Redirect from="/auth" to="/account" exact/>}
+                {!this.state.token && <Route path="/auth" component={ AuthPage }/>} 
+                {this.state.token && <Route path="/account" component={ AccountPage }/>}
+                {!this.state.token && <Redirect from="/account" to="/auth" exact/>}
+                <Route path="/home" component={ home }/>
+              </Switch>
+            </main>
+          </AuthContext.Provider>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- views/src/components/navigation/MainNavigation.js
+++ views/src/components/navigation/MainNavigation.js
@@ -6,9 +6,8 @@ import AuthContext from '../../context/auth-context'
 
 const MainNavigation = props => {
     return (
-<<<<<<< HEAD
         <AuthContext.Consumer>
-            {() => {
+            {(context) => {
                 return (
                     <header className="navbar navbar-perso navbar-expand-sm bg-light navbar-light sticky-top">
                         <div>
@@ -18,10 +17,16 @@ const MainNavigation = props => {
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
+                            <p>{context.token}</p>
                             <div className="navbar-collapse collapse" id="collapsingNavbar">
                                 <ul className="navbar-nav">
-                                    <li className="nav-item"><NavLink to="/auth">Authentification</NavLink></li>
-                                    <li className="nav-item"><NavLink to="/account">Account</NavLink></li>
+                                    <li className="nav-item"><NavLink to="/home">Home</NavLink></li>
+                                    {!context.token && (
+                                        <li className="nav-item"><NavLink to="/auth">Authentification</NavLink></li>
+                                    )}
+                                    {context.token && (
+                                        <li className="nav-item"><NavLink to="/account">Account</NavLink></li>
+                                    )}
                                 </ul>
                             </div>
                         </nav>
@@ -29,25 +34,6 @@ const MainNavigation = props => {
                 )
             }}
         </AuthContext.Consumer>
-=======
-        <header className="navbar navbar-perso navbar-expand-sm bg-light navbar-light sticky-top">
-            <div>
-                <NavLink to="/"><h1 className="navbar-brand abs">HYPERTUBE</h1></NavLink>
-            </div>
-            <nav className="main-navigation-item">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="navbar-collapse collapse" id="collapsingNavbar">
-                    <ul className="navbar-nav">
-                        <li className="nav-item"><NavLink to="/home">Home</NavLink></li>
-                        <li className="nav-item"><NavLink to="/auth">Authentification</NavLink></li>
-                        <li className="nav-item"><NavLink to="/account">Account</NavLink></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
->>>>>>> 9ed3d9a424cbb33cb40a9cde085dc2b519368e94
     );
 };
 export default MainNavigation;
